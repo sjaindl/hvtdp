f12f34e51c47f78f8f94a19ab9a34a4a0eda66e7
Stefan Jaindl <jaindl.stefan@gmail.com>
2018-06-12 08:31:07 +0200

    Add routing, team page, initial text & assets

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FlexLayoutModule } from '@angular/flex-layout'
 /*import { MatButtonModule } from '@angular/material'; */
+import { MatToolbar, MatToolbarModule } from '@angular/material';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
@@ -11,18 +12,21 @@ import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 
 import 'hammerjs';
+import { TeamComponent } from './team/team.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    TeamComponent
   ],
   imports: [
     BrowserModule ,
     BrowserAnimationsModule,
     /* MatButtonModule, */
+    MatToolbarModule,
     FlexLayoutModule, 
     AppRoutingModule,
   ],
