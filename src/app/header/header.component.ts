c09ff549c4a74cd7b9a08bf2c776f3459f6cccaf
Stefan Jaindl <jaindl.stefan@gmail.com>
2018-06-24 20:36:35 +0200

    Bug: Fix header side navigation overlay

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Renderer2, ViewChild } from '@angular/core';
+import { MatSidenav } from '@angular/material';
 
 @Component({
   selector: 'app-header',
@@ -6,6 +7,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+
+   @ViewChild(MatSidenav)
+    private sidenav: MatSidenav;
+
+    ngAfterContentInit() {
+        this.sidenav._animationStarted.subscribe(() => {
+            this.setSideNavigationWidth();
+        });
+    }
+
+   width = '0px'
+
+   setSideNavigationWidth() {
+    this.width = this.sidenav.opened ? '100%' : '0px'
+   }
 
   constructor() { }
 
