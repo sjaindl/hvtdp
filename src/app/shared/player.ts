84d3c75c8880592cea7ae3a8d6b6f51c1e88e587
Stefan Jaindl <jaindl.stefan@gmail.com>
2018-07-08 18:30:24 +0200

    feat: Finish team component

--- src/app/shared/player.ts
+++ src/app/shared/player.ts
@@ -1,24 +1,228 @@
 export class Player {
-    firstName: String
-    lastName: String
-    position: String
-    memberSinceYear: Number
-    imagePath: String
+    firstName: string
+    lastName: string
+    position: string
+    memberSinceYear: number
+    imagePath: string
 }
 
 export const PLAYERS : Player[] = [
+    {
+      firstName: "Horst",
+      lastName: 'Bohacek',
+      position: "Tormann",
+      memberSinceYear: 2014,
+      imagePath: '../../assets/images/team/BohacekH.JPG'
+    }, 
     {
       firstName: "Christian",
       lastName: 'Schnitter',
       position: "Tormann",
       memberSinceYear: 2015,
-      imagePath: '../../assets/images/schnitti.JPG'
+      imagePath: '../../assets/images/team/Schnitter.JPG'
     }, 
+    {
+      firstName: "Dimeo",
+      lastName: 'Rene',
+      position: "Verteidiger",
+      memberSinceYear: 2018,
+      imagePath: '/assets/images/team/Dimeo.JPG'
+    },
+    {
+      firstName: "Stefan",
+      lastName: 'Fladenhofer',
+      position: "Verteidiger",
+      memberSinceYear: 2016,
+      imagePath: '/assets/images/team/Fladenhofer.JPG'
+    },
+    {
+      firstName: "Jaindl",
+      lastName: 'Stefan',
+      position: "Verteidiger",
+      memberSinceYear: 2014,
+      imagePath: '/assets/images/team/Jaindl.JPG'
+    },
+    {
+      firstName: "Thomas",
+      lastName: 'Koinegg',
+      position: "Verteidiger",
+      memberSinceYear: 2014,
+      imagePath: '/assets/images/team/Koinegg.JPG'
+    },
+    {
+      firstName: "Robert",
+      lastName: 'Ronegg',
+      position: "Verteidiger",
+      memberSinceYear: 2016,
+      imagePath: '/assets/images/team/Ronegg.JPG'
+    },
     {
       firstName: "Hrovje",
       lastName: 'Sincek',
       position: "Verteidiger",
       memberSinceYear: 2014,
-      imagePath: '/assets/images/sinci.JPG'
+      imagePath: '/assets/images/team/Sincek.JPG'
+    },
+    {
+      firstName: "Michael",
+      lastName: 'Thomann',
+      position: "Verteidiger",
+      memberSinceYear: 2015,
+      imagePath: '/assets/images/team/Thomann.JPG'
+    },
+    {
+      firstName: "Albert",
+      lastName: 'Ademi',
+      position: "Mittelfeld",
+      memberSinceYear: 2014,
+      imagePath: '/assets/images/team/Ademi.JPG'
+    },
+    {
+      firstName: "Patrick",
+      lastName: 'Gödl',
+      position: "Mittelfeld",
+      memberSinceYear: 2018,
+      imagePath: '/assets/images/team/Goedl.JPG'
+    },
+    {
+      firstName: "Erik",
+      lastName: 'Hilberger',
+      position: "Mittelfeld",
+      memberSinceYear: 2014,
+      imagePath: '/assets/images/team/Hilberger.JPG'
+    },
+    {
+      firstName: "Herbert",
+      lastName: 'Kohlberger',
+      position: "Mittelfeld",
+      memberSinceYear: 2017,
+      imagePath: '/assets/images/team/Kohlberger.JPG'
+    },
+    {
+      firstName: "Mario",
+      lastName: 'Kügerl',
+      position: "Mittelfeld",
+      memberSinceYear: 2014,
+      imagePath: '/assets/images/team/Kuegerl.JPG'
+    },
+    {
+      firstName: "Rene",
+      lastName: 'Nöstel',
+      position: "Mittelfeld",
+      memberSinceYear: 2017,
+      imagePath: '/assets/images/team/Noestel.JPG'
+    },
+    {
+      firstName: "Michael",
+      lastName: 'Primus',
+      position: "Mittelfeld",
+      memberSinceYear: 2014,
+      imagePath: '/assets/images/team/Primus.JPG'
+    },
+    {
+      firstName: "Florian",
+      lastName: 'Puchleitner',
+      position: "Mittelfeld",
+      memberSinceYear: 2017,
+      imagePath: '/assets/images/team/Puchleitner.JPG'
+    },
+    {
+      firstName: "Lukas",
+      lastName: 'Ratswohl',
+      position: "Mittelfeld",
+      memberSinceYear: 2017,
+      imagePath: '/assets/images/team/Ratswohl.JPG'
+    },
+    {
+      firstName: "Nico",
+      lastName: 'Reinbacher',
+      position: "Mittelfeld",
+      memberSinceYear: 2016,
+      imagePath: '/assets/images/team/Reinbacher.JPG'
+    },
+    {
+      firstName: "Markus",
+      lastName: 'Sackl',
+      position: "Mittelfeld",
+      memberSinceYear: 2014,
+      imagePath: '/assets/images/team/Sackl.JPG'
+    },
+    {
+      firstName: "Sebastian",
+      lastName: 'Steiner',
+      position: "Mittelfeld",
+      memberSinceYear: 2017,
+      imagePath: '/assets/images/team/Steiner.JPG'
+    },
+    {
+      firstName: "Andreas",
+      lastName: 'Stelzer',
+      position: "Mittelfeld",
+      memberSinceYear: 2014,
+      imagePath: '/assets/images/team/Stelzer.JPG'
+    },
+    {
+      firstName: "Patrick",
+      lastName: 'Wolf',
+      position: "Mittelfeld",
+      memberSinceYear: 2018,
+      imagePath: '/assets/images/team/Wolf.JPG'
+    },
+
+    {
+      firstName: "Philipp",
+      lastName: 'Bohacek',
+      position: "Stürmer",
+      memberSinceYear: 2014,
+      imagePath: '/assets/images/team/BohacekP.JPG'
+    },
+    {
+      firstName: "Daniel",
+      lastName: 'Fabian',
+      position: "Stürmer",
+      memberSinceYear: 2014,
+      imagePath: '/assets/images/team/Fabian.JPG'
+    },
+    {
+      firstName: "Philipp",
+      lastName: 'Haberl',
+      position: "Stürmer",
+      memberSinceYear: 2017,
+      imagePath: '/assets/images/team/Haberl.JPG'
+    },
+    {
+      firstName: "David",
+      lastName: 'Kovacs',
+      position: "Stürmer",
+      memberSinceYear: 2016,
+      imagePath: '/assets/images/team/Kovacs.JPG'
+    },
+    {
+      firstName: "Klaus",
+      lastName: 'Loscher',
+      position: "Stürmer",
+      memberSinceYear: 2016,
+      imagePath: '/assets/images/team/Loscher.JPG'
+    },
+    {
+      firstName: "Manuel",
+      lastName: 'Pichler',
+      position: "Stürmer",
+      memberSinceYear: 2014,
+      imagePath: '/assets/images/team/Pichler.JPG'
+    },
+    {
+      firstName: "Patrick",
+      lastName: 'Reinbacher',
+      position: "Stürmer",
+      memberSinceYear: 2015,
+      imagePath: '/assets/images/team/Reinbacher.JPG'
+    },
+    {
+      firstName: "Florian",
+      lastName: 'Schriebl',
+      position: "Stürmer",
+      memberSinceYear: 2016,
+      imagePath: '/assets/images/team/Schriebl.JPG'
     }
-];
+  ];
--- src/app/team/team.component.ts
+++ src/app/team/team.component.ts
@@ -7,15 +7,40 @@ import { PLAYERS, Player } from '../shared/player';
   styleUrls: ['./team.component.css']
 })
 export class TeamComponent implements OnInit {
+  
+  teamPositionSortOrder = new Map<string, number>();
 
   players: Player[];
 
   constructor() {
-
-   }
-
-  ngOnInit() {
-    this.players = PLAYERS
+    this.teamPositionSortOrder.set("Tormann", 1);
+    this.teamPositionSortOrder.set("Verteidiger", 2);
+    this.teamPositionSortOrder.set("Mittelfeld", 3);
+    this.teamPositionSortOrder.set("Stürmer", 4);
   }
 
+  ngOnInit() {
+    //players should first be sorted by position, then by last name
+    this.players = PLAYERS.sort((a, b) => {
+      var aPositionSortOrder = this.teamPositionSortOrder.get(a.position)
+      var bPositionSortOrder = this.teamPositionSortOrder.get(b.position)
+
+      if (aPositionSortOrder < bPositionSortOrder) {
+        return -1;
+      } 
+      else if (aPositionSortOrder > bPositionSortOrder) {
+        return 1;
+      }
+      else {
+        if (a.lastName < b.lastName) {
+          return -1;
+        } 
+        else if (a.lastName > b.lastName) {
+          return 1;
+        } else {
+          return 0;
+        }
+      }
+    });
+  }
 }
