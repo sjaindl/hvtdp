5d76a2089ddb686726b692787d52ffdb5be29b67
Stefan Jaindl <jaindl.stefan@gmail.com>
2018-07-23 23:58:57 +0200

    feat: responsive carousel

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 import { NEWS, News } from '../shared/news';
+import { DeviceDetectorService } from '../../../node_modules/ngx-device-detector';
 
 @Component({
   selector: 'app-home',
@@ -8,15 +9,52 @@ import { NEWS, News } from '../shared/news';
 })
 export class HomeComponent implements OnInit {
 
-  title = 'HV TDP Stainz';
-
   items: Array<any> = []
+  isMobile = null
+  carouselWidth = null
+  imageWidth = null
+  imageHeight = null
   
-  constructor() { 
+  constructor(private deviceService: DeviceDetectorService) { 
     this.items = NEWS
   }
 
   ngOnInit() {
+    /*
+    this.deviceInfo = this.deviceService.getDeviceInfo();
+
+    device info holds the following properties:
+    browser
+    os
+    device (mobile/table/desktop)
+    userAgent
+    os_version
+    */
+    this.checkDevice()
+    this.isMobile || window.innerWidth < 641 ? this.setMobile() : this.setDesktop()
   }
 
+  checkDevice() {
+    this.isMobile = this.deviceService.isMobile()
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event) {
+    this.checkDevice()
+    event.target.innerWidth < 641 ? this.setMobile() : this.setDesktop()
+  }
+
+  setMobile() {
+    this.setAttributes(280, 280, 196)
+  }
+
+  setDesktop() {
+    this.setAttributes(700, 700, 490)
+  }
+
+  setAttributes(carouselWidth, imageWidth, imageHeight) {
+    this.carouselWidth = carouselWidth
+    this.imageWidth = imageWidth
+    this.imageHeight = imageHeight
+  }
 }
