67523976d01771d86ab535ef9d82352c8a1a605a
Stefan Jaindl <jaindl.stefan@gmail.com>
2020-10-28 23:29:07 +0100

    Remove team position ordering

--- src/app/team/team.component.ts
+++ src/app/team/team.component.ts
@@ -10,19 +10,11 @@ import { Title, Meta } from '@angular/platform-browser'
   styleUrls: ['./team.component.css']
 })
 export class TeamComponent implements OnInit {
-  
-  teamPositionSortOrder = new Map<string, number>()
-
   squadPlayers: Player[]
   activePlayers: Player[]
   imageBaseUrl: String
 
-  constructor(private mysqlService: MysqlService, private titleService: Title, private metaTagService: Meta) {
-    this.teamPositionSortOrder.set("Tormann", 1)
-    this.teamPositionSortOrder.set("Verteidiger", 2)
-    this.teamPositionSortOrder.set("Mittelfeld", 3)
-    this.teamPositionSortOrder.set("Stürmer", 4)
-  }
+  constructor(private mysqlService: MysqlService, private titleService: Title, private metaTagService: Meta) { }
   
   ngOnInit() {
     this.imageBaseUrl = baseUrlImages
@@ -30,24 +22,13 @@ export class TeamComponent implements OnInit {
     this.mysqlService.getSquadPlayers().subscribe( players => {
       //players should first be sorted by position, then by last name
       this.squadPlayers = players.sort((a, b) => {
-        var aPositionSortOrder = this.teamPositionSortOrder.get(a.position)
-        var bPositionSortOrder = this.teamPositionSortOrder.get(b.position)
-  
-        if (aPositionSortOrder < bPositionSortOrder) {
+        if (a.lastName < b.lastName) {
           return -1;
         } 
-        else if (aPositionSortOrder > bPositionSortOrder) {
+        else if (a.lastName > b.lastName) {
           return 1;
-        }
-        else {
-          if (a.lastName < b.lastName) {
-            return -1;
-          } 
-          else if (a.lastName > b.lastName) {
-            return 1;
-          } else {
-            return 0;
-          }
+        } else {
+          return 0;
         }
       });
     })
@@ -55,24 +36,13 @@ export class TeamComponent implements OnInit {
     this.mysqlService.getActivePlayers().subscribe( players => {
       //players should first be sorted by position, then by last name
       this.activePlayers = players.sort((a, b) => {
-        var aPositionSortOrder = this.teamPositionSortOrder.get(a.position)
-        var bPositionSortOrder = this.teamPositionSortOrder.get(b.position)
-  
-        if (aPositionSortOrder < bPositionSortOrder) {
+        if (a.lastName < b.lastName) {
           return -1;
         } 
-        else if (aPositionSortOrder > bPositionSortOrder) {
+        else if (a.lastName > b.lastName) {
           return 1;
-        }
-        else {
-          if (a.lastName < b.lastName) {
-            return -1;
-          } 
-          else if (a.lastName > b.lastName) {
-            return 1;
-          } else {
-            return 0;
-          }
+        } else {
+          return 0;
         }
       });
     })
