c4cbd86260ce48e6b0894c3c83dda67f9f9b2cda
Stefan Jaindl <stefan.jaindl@alturos.com>
2021-10-28 15:36:35 +0200

    Fix clicks not working due to navigation bar

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,31 +1,14 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
-import { MatSidenav } from '@angular/material/sidenav';
+import { Component } from '@angular/core'
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent implements OnInit {
-
-   @ViewChild(MatSidenav)
-    private sidenav: MatSidenav;
-
-    ngAfterContentInit() {
-        this.sidenav._animationStarted.subscribe(() => {
-          this.setSideNavigationWidth();
-        });
-    }
-
+export class HeaderComponent {
    width = '0px'
 
-   setSideNavigationWidth() {
-    this.width = this.sidenav.opened ? '100%' : '0px';
-   }
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+   navChanged(opened: boolean) {
+     this.width = opened ? '100%' : '0px'
+   }  
 }
