7dd66953c5b7b6560c276c8948872d4873c2b964
Stefan Jaindl <jaindl.stefan@gmail.com>
2024-03-15 10:26:27 +0100

    feat(10-jahresfeier): Replace form with eventim iFrame

--- src/app/services/mysql.service.ts
+++ src/app/services/mysql.service.ts
@@ -168,30 +168,6 @@ export class MysqlService {
     })
   }
 
-  public postTenYearJubilaeum(firstname: string, lastname: string, mail: string, address: string, ticketCount: number): Observable<any> {
-    var url = 'https://www.hvtdpstainz.at/api/postJubilaeum.php?'
-    url += 'firstname=' + firstname
-    url += '&lastname=' + lastname
-    url += '&mail=' + mail
-    url += '&address=' + address
-    url += '&ticketCount=' + ticketCount
-    url += '&useragent=' + window.navigator.userAgent
-    url += '&language=' + window.navigator.language
-    url += '&platform=' + window.navigator.platform
-
-    return this.http.get<any>(url, {
-      params: new HttpParams()
-      .append('firstname', firstname)
-      .append('lastname', lastname)
-      .append('mail', mail)
-      .append('address', address)
-      .append('ticketCount',  ""+ticketCount)
-      .append('useragent', window.navigator.userAgent)
-      .append('language', window.navigator.language)
-      .append('platform', window.navigator.platform)
-    })
-  }
-
   public postGoldenShotRegistration(firstName: string, lastName: string, mail: string, phone: string, photo: string): Observable<any> {
     var url = 'https://www.hvtdpstainz.at/api/postGoldenShot.php?'
     url += '&firstName=' + firstName
--- src/app/zehnjahresfeier/zehnjahresfeier.component.ts
+++ src/app/zehnjahresfeier/zehnjahresfeier.component.ts
@@ -1,8 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Feedback } from '../shared/feedback'
-import { FormBuilder, FormGroup, Validators } from '@angular/forms'
 import { Title, Meta } from '@angular/platform-browser'
-import { MysqlService } from '../services/mysql.service'
 
 @Component({
   selector: 'app-zehnjahresfeier',
@@ -10,50 +7,9 @@ import { MysqlService } from '../services/mysql.service'
   styleUrls: ['./zehnjahresfeier.component.css']
 })
 export class ZehnjahresfeierComponent implements OnInit {
-  feedbackForm: FormGroup
-  feedback: Feedback
-  submittedFeedback = null
-
-  formErrors = {
-    'firstname': '',
-    'lastname': '',
-    'email': '',
-    'address': '',
-    'ticketcount': ''
-  }
-
-  validationMessages = {
-    'firstname': {
-      'required':      'Der Vorname ist verpflichtend.',
-      'minlength':     'Der Vorname muss mindestens 2 Zeichen lang sein.',
-      'maxlength':     'Der Vorname darf maximal 25 Zeichen lang sein.'
-    },
-    'lastname': {
-      'required':      'Der Nachname ist verpflichtend.',
-      'minlength':     'Der Nachname muss mindestens 2 Zeichen lang sein.',
-      'maxlength':     'Der Nachname darf maximal 25 Zeichen lang sein.'
-    },
-    'address': {
-      'required':      'Die Adresse ist verpflichtend.',
-      'minlength':     'Die Adresse muss mindestens 10 Zeichen lang sein.',
-      'maxlength':     'Die Adresse darf maximal 100 Zeichen lang sein.'
-    },
-    'email': {
-      'required':      'Die E-Mail ist verpflichtend.',
-      'email':         'E-Mail hat kein gültiges Format.'
-    },
-    'ticketcount': {
-      'pattern':       'Die Ticketanzahl ist ungültig.',
-      'required':      'Die Ticketanzahl ist verpflichtend.'
-    },
-  }
-
   constructor(
-    private formBuilder: FormBuilder,
     private titleService: Title,
-    private metaTagService: Meta,
-    private mysqlService: MysqlService) {
-    this.createForm()
+    private metaTagService: Meta) {
   }
 
   ngOnInit() {
@@ -67,63 +23,5 @@ export class ZehnjahresfeierComponent implements OnInit {
       { name: 'author', content: 'Stefan Jaindl' },
       { charset: 'UTF-8' }
     ])
-  }
-
-  createForm() {
-    this.feedbackForm = this.formBuilder.group({
-      firstname: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(25)] ],
-      lastname: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(25)] ],
-      address: ['', [Validators.required, Validators.minLength(10), Validators.maxLength(100)] ],
-      email: ['', [Validators.required, Validators.email] ],
-      ticketcount: ['', [Validators.minLength(1), Validators.pattern("[0-9]*")] ],
-    })
-
-    this.onValueChanged() // (re)set validation messages now
-
-    this.feedbackForm.valueChanges
-      .subscribe(data => this.onValueChanged(data))
-  }
-
-  onValueChanged(data?: any) {
-    if (!this.feedbackForm) {
-      return
-    }
-
-    for (const field in this.formErrors) {
-      this.formErrors[field] = '' // clear previous error message (if any)
-      const control = this.feedbackForm.get(field)
-      if (control && control.dirty && !control.valid) {
-        const messages = this.validationMessages[field]
-        for (const key in control.errors) {
-          this.formErrors[field] += messages[key] + ' '
-        }
-      }
-    }
-  }
-
-  onSubmit() {
-    this.feedback = this.feedbackForm.value
-    this.submittedFeedback = this.feedback
-
-    console.log(`send to server: ${this.submittedFeedback.firstname}, ${this.submittedFeedback.lastname},
-    ${this.submittedFeedback.email}, ${this.submittedFeedback.address}, ${this.submittedFeedback.ticketcount}`)
-
-    this.mysqlService.postTenYearJubilaeum(
-      this.submittedFeedback.firstname,
-      this.submittedFeedback.lastname,
-      this.submittedFeedback.email,
-      this.submittedFeedback.address,
-      this.submittedFeedback.ticketcount
-      ).subscribe(result => {
-        console.log(`response from server: ${result}`)
-    })
-
-    this.feedbackForm.reset({
-      firstname: '',
-      lastname: '',
-      email: '',
-      address: '',
-      ticketcount: ''
-    })
   }
 }
