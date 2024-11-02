30c0799c3d778c0bd5410d662f0d08cb1a297e1b
Stefan Jaindl <jaindl.stefan@gmail.com>
2024-11-02 23:55:27 +0100

    feat(goalofseason): Implement goal of season voting

---
+++ serverscripts/api/postGoalOfTheSeasonVote.php
@@ -0,0 +1,32 @@
+<?php require 'constants.php';
+
+    header('Content-Type: text/plain; charset=utf-8');
+    header("Access-Control-Allow-Origin: *");
+    header("Access-Control-Allow-Headers: *");
+    header("Access-Control-Allow-Methods: GET, POST");
+
+    postGoalOfTheSeasonVote($dbname, $dbuser, $dbpass, $dbhost);
+
+    function postGoalOfTheSeasonVote($name, $user, $pass, $host) {
+        $con = @mysqli_connect($host, $user, $pass, $name);
+
+        if (!$con) {
+            echo "Error: " . mysqli_connect_error();
+            exit();
+        }
+
+        $season = $_GET['season'];
+        $player = $_GET['player'];
+
+        $sql = "UPDATE `GoalOfTheSeason` SET `votes` = `votes` + 1 WHERE `season` = '$season' AND `player` = '$player';";
+
+        if ($con->query($sql) === TRUE) {
+            echo '{"result": "Updated record successfully!"}';
+        } else {
+            echo '{"Error": "' . $con->error . '"';
+        }
+
+        // Close connection
+        mysqli_close ($con);
+    }
+?>
--- src/app/goaloftheseason/goaloftheseason.component.ts
+++ src/app/goaloftheseason/goaloftheseason.component.ts
@@ -6,6 +6,10 @@ import { MysqlService } from '../services/mysql.service'
 import { DeviceDetectorService } from 'ngx-device-detector'
 import { Title, Meta } from '@angular/platform-browser'
 import { ActivatedRoute } from '@angular/router'
+import { GoalOfTheSeason } from '../shared/goaloftheseason'
+import { CookieService } from 'ngx-cookie-service'
+
+const GOAL_OF_SEASON_VOTING_COOKIE_KEY = 'goal_of_season_voted'
 
 @Component({
   selector: 'app-goaloftheseason',
@@ -20,10 +24,13 @@ export class GoalOfTheSeasonComponent implements OnInit {
   players: Player[]
   imageBaseUrl: String
   isMobile = null
+  canVote = false
+  goalOfSeasonOptions: GoalOfTheSeason[]
 
   private sub: any
 
   constructor(
+    private cookieService: CookieService,
     private mysqlService: MysqlService,
     private route: ActivatedRoute,
     private cdr: ChangeDetectorRef,
@@ -32,6 +39,7 @@ export class GoalOfTheSeasonComponent implements OnInit {
     private metaTagService: Meta) { }
 
   ngOnInit() {
+    this.canVote = !this.cookieService.check(GOAL_OF_SEASON_VOTING_COOKIE_KEY);
     this.imageBaseUrl = baseUrlImages
 
     this.checkDevice()
@@ -59,6 +67,21 @@ export class GoalOfTheSeasonComponent implements OnInit {
 
       this.mysqlService.getPlayers().subscribe(players => {
         this.players = players
+      })
+
+      this.mysqlService.getGoalOfTheSeason().subscribe( options => {
+        this.goalOfSeasonOptions = options.filter((a) => {
+          return a.season === this.season
+        }).sort((a, b) => {
+          if (a.player < b.player) {
+            return -1;
+          }
+          else if (a.player > b.player) {
+            return 1;
+          } else {
+            return 0;
+          }
+        });
       })
     })
 
@@ -101,5 +124,14 @@ export class GoalOfTheSeasonComponent implements OnInit {
 
   reload() {
     window.location.reload();
+  }
+
+  saveVote(vote: GoalOfTheSeason) {
+    this.mysqlService.postGoalOfTheSeasonVote(vote.season, vote.player).subscribe(result => {
+      console.log('posted vote', JSON.stringify(vote), vote.player, vote.season)
+      this.cookieService.set(GOAL_OF_SEASON_VOTING_COOKIE_KEY, 'true', 1 / 24) // (peristent) cookie expires after 1 hour
+      console.log(`response from server: ${JSON.stringify(result)}`)
+      this.canVote = false
+    })
   }
 }
