4ddfdb5a36d71c64ffd1f3635c79617a75c36e9c
Stefan Jaindl <jaindl.stefan@gmail.com>
2025-11-29 16:50:29 +0100

    feat(home): Add new ticker component

---
+++ src/app/ticker/ticker.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-ticker',
+  templateUrl: './ticker.component.html',
+  styleUrls: ['./ticker.component.css']
+})
+export class TickerComponent implements OnInit {
+
+  @Input() text: string = "";
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
