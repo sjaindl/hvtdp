f3bf143224e809cf05dae0fb16b043fedfdf0fa5
Stefan Jaindl <jaindl.stefan@gmail.com>
2025-12-06 19:53:53 +0100

    feat(team): New team UI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,75 +1,75 @@
-import { AppRoutingModule } from './app-routing/app-routing.module';
-import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatIconModule } from '@angular/material/icon';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatListModule } from '@angular/material/list';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatDividerModule } from '@angular/material/divider';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatGridListModule } from '@angular/material/grid-list';
-import { MatInputModule } from '@angular/material/input';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatSlideToggleModule } from '@angular/material/slide-toggle';
-import { MatSelectModule } from '@angular/material/select';
-import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatExpansionModule } from '@angular/material/expansion';
-import { MatPaginatorModule } from '@angular/material/paginator';
-import { MatTableModule } from '@angular/material/table';
-import { MatSortModule } from '@angular/material/sort';
-import { MatSnackBarModule, MAT_SNACK_BAR_DEFAULT_OPTIONS } from '@angular/material/snack-bar';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
 import { MatMenuModule } from '@angular/material/menu';
-import { MatButtonModule } from '@angular/material/button';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MAT_SNACK_BAR_DEFAULT_OPTIONS, MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatSortModule } from '@angular/material/sort';
+import { MatTableModule } from '@angular/material/table';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppRoutingModule } from './app-routing/app-routing.module';
 
-import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
-import { LocationStrategy, HashLocationStrategy } from '@angular/common';
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 import { provideHttpClient, withInterceptorsFromDi, withJsonpSupport } from '@angular/common/http';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { GoogleMapsModule } from '@angular/google-maps';
-import { NgcCookieConsentConfig } from 'ngx-cookieconsent';
 import { CookieService } from 'ngx-cookie-service';
+import { NgcCookieConsentConfig } from 'ngx-cookieconsent';
 
-import { AppComponent } from './app.component';
 import { RedirectGuard } from './app-routing/redirect-guard';
+import { AppComponent } from './app.component';
 import { MysqlService } from './services/mysql.service';
 import { StatisticsService } from './services/statistics.service';
 
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
 import { AboutComponent } from './about/about.component';
 import { AdventComponent } from './advent/advent.component';
+import { BigdartsComponent } from './bigdarts/bigdarts.component';
+import { BocciaComponent } from './boccia/boccia.component';
+import { ChampionshipComponent } from './championship/championship.component';
 import { ComedyhirtenComponent } from './comedyhirten/comedyhirten.component';
+import { ContactComponent } from './contact/contact.component';
 import { DocumentsComponent } from './documents/documents.component';
 import { DonationsComponent } from './donations/donations.component';
+import { DuckrunComponent } from './duckrun/duckrun.component';
+import { FanshopComponent } from './fanshop/fanshop.component';
+import { FooterComponent } from './footer/footer.component';
 import { GalleryComponent } from './gallery/gallery.component';
 import { GoalOfTheSeasonComponent } from './goaloftheseason/goaloftheseason.component';
 import { GoldenshotVotingComponent } from './goldenshot-voting/goldenshot-voting.component';
+import { GoldenshotComponent } from './goldenshot/goldenshot.component';
+import { HallenturnierComponent } from './hallenturnier/hallenturnier.component';
 import { HalloffameComponent } from './halloffame/halloffame.component';
+import { HallofpappComponent } from './hallofpapp/hallofpapp.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { HvtdpImageComponent } from './hvtdp-image/hvtdp-image.component';
+import { ImagesliderComponent } from './imageslider/imageslider.component';
+import { KleinfeldTurnierComponent } from './kleinfeldturnier/kleinfeldturnier.component';
 import { MembershipComponent } from './membership/membership.component';
-import { PlayerOfTheSeasonComponent } from './playeroftheseason/playeroftheseason.component';
+import { NewsComponent } from './news/news.component';
+import { PassworddialogComponent } from './passworddialog/passworddialog.component';
 import { SafePipe } from './pipes/SafePipe';
+import { PlayerOfTheSeasonComponent } from './playeroftheseason/playeroftheseason.component';
 import { SocialmediaComponent } from './socialmedia/socialmedia.component';
 import { StadionFestComponent } from './stadionfest/stadionfest.component';
 import { StartingelevenComponent } from './startingeleven/startingeleven.component';
 import { SurveysComponent } from './surveys/surveys.component';
 import { TeamComponent } from './team/team.component';
-import { ZehnjahresfeierComponent } from './zehnjahresfeier/zehnjahresfeier.component';
-import { HomeComponent } from './home/home.component';
-import { NewsComponent } from './news/news.component';
-import { ChampionshipComponent } from './championship/championship.component';
-import { KleinfeldTurnierComponent } from './kleinfeldturnier/kleinfeldturnier.component';
-import { DuckrunComponent } from './duckrun/duckrun.component';
-import { GoldenshotComponent } from './goldenshot/goldenshot.component';
-import { PassworddialogComponent } from './passworddialog/passworddialog.component';
 import { TickerComponent } from './ticker/ticker.component';
-import { ImagesliderComponent } from './imageslider/imageslider.component';
-import { HvtdpImageComponent } from './hvtdp-image/hvtdp-image.component';
-import { BigdartsComponent } from './bigdarts/bigdarts.component';
-import { ContactComponent } from './contact/contact.component';
-import { BocciaComponent } from './boccia/boccia.component';
-import { FanshopComponent } from './fanshop/fanshop.component';
-import { HallenturnierComponent } from './hallenturnier/hallenturnier.component';
-import { HallofpappComponent } from './hallofpapp/hallofpapp.component';
+import { ZehnjahresfeierComponent } from './zehnjahresfeier/zehnjahresfeier.component';
 
 const cookieConfig: NgcCookieConsentConfig = {
   cookie: {
@@ -84,7 +84,7 @@ const cookieConfig: NgcCookieConsentConfig = {
     },
   },
   theme: 'edgeless',
-    type: 'opt-out',
+  type: 'opt-out',
 };
 
 @NgModule({
@@ -105,7 +105,6 @@ const cookieConfig: NgcCookieConsentConfig = {
     StadionFestComponent,
     StartingelevenComponent,
     SurveysComponent,
-    TeamComponent,
     ZehnjahresfeierComponent,
   ],
   imports: [
@@ -153,6 +152,7 @@ const cookieConfig: NgcCookieConsentConfig = {
     ContactComponent,
     BocciaComponent,
     FanshopComponent,
+    TeamComponent,
   ],
   bootstrap: [AppComponent],
   providers: [
--- src/app/team/team.component.ts
+++ src/app/team/team.component.ts
@@ -1,19 +1,31 @@
 import { Component, OnInit } from '@angular/core';
-import { Player } from '../shared/player';
-import { baseUrlImages } from '../shared/baseurls';
+import { MatIconModule } from '@angular/material/icon';
+import { Meta, Title } from '@angular/platform-browser';
+import { HvtdpImageComponent } from '../hvtdp-image/hvtdp-image.component';
 import { MysqlService } from '../services/mysql.service';
-import { Title, Meta } from '@angular/platform-browser';
+import { baseUrlImages } from '../shared/baseurls';
+import { Player } from '../shared/player';
+
+interface PositionGroup {
+  position: string;
+  icon: string;
+  players: Player[];
+}
 
 @Component({
-    selector: 'app-team',
-    templateUrl: './team.component.html',
-    styleUrls: ['./team.component.css'],
-    standalone: false
+  selector: 'app-team',
+  templateUrl: './team.component.html',
+  styleUrls: ['./team.component.css'],
+  standalone: true,
+  imports: [HvtdpImageComponent, MatIconModule],
 })
 export class TeamComponent implements OnInit {
-  squadPlayers: Player[];
-  activePlayers: Player[];
-  imageBaseUrl: String;
+  squadPlayers: Player[] = [];
+  activePlayers: Player[] = [];
+  positionGroups: PositionGroup[] = [];
+  imageBaseUrl: string = baseUrlImages as string;
+
+  private readonly positionOrder: string[] = ['Tormann', 'Verteidiger', 'Mittelfeld', 'Stürmer'];
 
   constructor(
     private mysqlService: MysqlService,
@@ -22,32 +34,13 @@ export class TeamComponent implements OnInit {
   ) {}
 
   ngOnInit() {
-    this.imageBaseUrl = baseUrlImages;
-
     this.mysqlService.getSquadPlayers().subscribe((players) => {
-      //players should first be sorted by position, then by last name
-      this.squadPlayers = players.sort((a, b) => {
-        if (a.lastName < b.lastName) {
-          return -1;
-        } else if (a.lastName > b.lastName) {
-          return 1;
-        } else {
-          return 0;
-        }
-      });
+      this.squadPlayers = this.sortPlayers(players);
+      this.buildPositionGroups();
     });
 
     this.mysqlService.getActivePlayers().subscribe((players) => {
-      //players should first be sorted by position, then by last name
-      this.activePlayers = players.sort((a, b) => {
-        if (a.lastName < b.lastName) {
-          return -1;
-        } else if (a.lastName > b.lastName) {
-          return 1;
-        } else {
-          return 0;
-        }
-      });
+      this.activePlayers = this.sortPlayers(players);
     });
 
     this.titleService.setTitle('HV TDP Stainz: Unsere Mannschaft');
@@ -64,5 +57,95 @@ export class TeamComponent implements OnInit {
       { name: 'author', content: 'Stefan Jaindl' },
       { charset: 'UTF-8' },
     ]);
+  }
+
+  private sortPlayers(players: Player[]): Player[] {
+    return [...(players ?? [])].sort((a, b) => {
+      const lastNameA = (a?.lastName || '').toLocaleLowerCase('de-DE');
+      const lastNameB = (b?.lastName || '').toLocaleLowerCase('de-DE');
+      return lastNameA.localeCompare(lastNameB, 'de-DE');
+    });
+  }
+
+  private buildPositionGroups(): void {
+    if (!this.squadPlayers?.length) {
+      this.positionGroups = [];
+      return;
+    }
+
+    const grouped = new Map<string, Player[]>();
+
+    this.squadPlayers.forEach((player) => {
+      const key = player.position || 'Weitere Positionen';
+      const currentGroup = grouped.get(key) ?? [];
+      currentGroup.push(player);
+      grouped.set(key, currentGroup);
+    });
+
+    this.positionGroups = Array.from(grouped.entries())
+      .sort((a, b) => this.comparePositions(a[0], b[0]))
+      .map(([position, players]) => ({
+        position,
+        icon: this.getPositionIcon(position),
+        players: this.sortPlayers(players),
+      }));
+  }
+
+  private comparePositions(a: string, b: string): number {
+    const indexA = this.positionOrder.indexOf(a);
+    const indexB = this.positionOrder.indexOf(b);
+    const normalizedA = indexA === -1 ? Number.MAX_SAFE_INTEGER : indexA;
+    const normalizedB = indexB === -1 ? Number.MAX_SAFE_INTEGER : indexB;
+
+    if (normalizedA !== normalizedB) {
+      return normalizedA - normalizedB;
+    }
+
+    return a.localeCompare(b, 'de-DE');
+  }
+
+  getPositionIcon(position: string): string {
+    switch (position) {
+      case 'Tormann':
+        return '🧤';
+      case 'Verteidiger':
+        return '🛡️';
+      case 'Mittelfeld':
+        return '⚡';
+      case 'Stürmer':
+        return '⚽';
+      default:
+        return '⭐';
+    }
+  }
+
+  getPlayerNumber(player: Player): number | undefined {
+    const jersey = player?.jerseyNumber ?? player?.number;
+    return jersey === undefined ? undefined : jersey;
+  }
+
+  getMemberSince(player: Player): string {
+    if (player?.memberSinceDate) {
+      return this.formatDate(player.memberSinceDate);
+    }
+
+    if (player?.memberSinceYear) {
+      return `${player.memberSinceYear}`;
+    }
+
+    return 'unbekannt';
+  }
+
+  private formatDate(dateString: string): string {
+    const parsedDate = new Date(dateString);
+    if (Number.isNaN(parsedDate.getTime())) {
+      return dateString;
+    }
+
+    return parsedDate.toLocaleDateString('de-DE', {
+      day: '2-digit',
+      month: '2-digit',
+      year: 'numeric',
+    });
   }
 }
