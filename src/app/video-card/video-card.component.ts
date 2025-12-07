71f3a5726528ec57cb8e678076ad3e41841b5440
Stefan Jaindl <jaindl.stefan@gmail.com>
2025-12-08 00:12:53 +0100

    fix(video): Fix url caching

--- src/app/video-card/video-card.component.ts
+++ src/app/video-card/video-card.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
 import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 import { HvtdpImageComponent } from '../hvtdp-image/hvtdp-image.component';
 
@@ -10,15 +10,18 @@ import { HvtdpImageComponent } from '../hvtdp-image/hvtdp-image.component';
   templateUrl: './video-card.component.html',
   styleUrls: ['./video-card.component.css'],
 })
-export class VideoCardComponent {
+export class VideoCardComponent implements OnChanges {
   @Input() videoUrl = '';
   @Input() scorerImage = '';
   @Input() scorerName = '';
   @Input() description = 'Video-Highlight';
+  safeUrl: SafeResourceUrl = '';
 
   constructor(private sanitizer: DomSanitizer) {}
 
-  get safeUrl(): SafeResourceUrl {
-    return this.sanitizer.bypassSecurityTrustResourceUrl(this.videoUrl || '');
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes['videoUrl']) {
+      this.safeUrl = this.sanitizer.bypassSecurityTrustResourceUrl(this.videoUrl || '');
+    }
   }
 }
